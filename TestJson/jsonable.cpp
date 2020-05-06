88d1e287f7491e950dee6d646715318c466196b3
jimmycoconut88 <jimmythenthusiast@gmail.com>
2020-05-06 20:22:20 +0700

    Commented and explained

--- TestJson/jsonable.cpp
+++ TestJson/jsonable.cpp
@@ -20,15 +20,15 @@ jsonable& jsonable::operator=(const jsonable &){
 }
 
 void jsonable::read(const QJsonObject &json){
-        if (json.contains("menu") && json["menu"].isArray()) {
-            QJsonArray menuArr = json["menu"].toArray();
-            menu.clear();
-            menu.reserve(menuArr.size());
-        for(int i = 0; i < menuArr.size(); ++i){
-            jsonable temp;
-               QJsonObject obj = menuArr[i].toObject();
+        if (json.contains("menu") && json["menu"].isArray()) {  //Find if an Array item named menu exist
+            QJsonArray menuArr = json["menu"].toArray();        //Convert it to a JSONArray obj to use easily
+            menu.clear();                                       //Clear memory
+            menu.reserve(menuArr.size());                       //alocate the needed space for the array
+        for(int i = 0; i < menuArr.size(); ++i){                //For loop to run through all of Jsonarray obj
+            jsonable temp;                                      //set a temporary storing variable
+               QJsonObject obj = menuArr[i].toObject();         //transfer an Array element into a Json obj
             if (obj.contains("name") && obj["name"].isString())
-                temp.name = obj["name"].toString();
+                temp.name = obj["name"].toString();             //use the sotring variable to store an element of the Json Obj, remember to declare the correct elements and variable type
             if (obj.contains("description") && obj["description"].isString())
                 temp.description = obj["description"].toString();
             if (obj.contains("category") && obj["category"].isString())
@@ -37,37 +37,37 @@ void jsonable::read(const QJsonObject &json){
                 temp.price = obj["price"].toDouble();
 
 
-            if (obj.contains("quantity") && obj["quantity"].isArray()) {
-                QJsonArray Jquantity = obj["quantity"].toArray();
+            if (obj.contains("quantity") && obj["quantity"].isArray()) {    //it is also possible to look for array element within an element
+                QJsonArray Jquantity = obj["quantity"].toArray();           //Convert the obj array element into Json array
                 temp.quantity.clear();
                 temp.quantity.reserve(Jquantity.size());
-                for (int j = 0; j < Jquantity.size(); ++j) {
-                    QJsonObject q = Jquantity[j].toObject();
-                    int quanObject = q["quantity"].toInt();
-                    temp.quantity.push_back(quanObject);
+                for (int j = 0; j < Jquantity.size(); ++j) {                //For loop running thru the json array
+                    QJsonObject q = Jquantity[j].toObject();                // store an array element into a Json obj (remember to convert to is object)
+                    int quanObject = q["quantity"].toInt();                 //Similar to the process above extract the Json Obj element u want
+                    temp.quantity.push_back(quanObject);                    //Store the info
                 }
             }
-            menu.push_back(temp);
+            menu.push_back(temp);                                           //Store the temporary obj into ur using variable
         }
     }
 }
 void jsonable::write(QJsonObject &json) const {
 
-    QJsonArray menuArr;
-    for (const jsonable &items : menu) {
-        QJsonObject temp;
-        temp["name"] = items.name;
+    QJsonArray menuArr;                                         //Declare a json array obj to store elements
+    for (const jsonable &items : menu) {                        //for loop to run through ur variable list that also store the list elements into items
+        QJsonObject temp;                                       //Declare temporary Json Obj to store wanted elements
+        temp["name"] = items.name;                              //Proceed to declare the elements name and store its data
         temp["description"] = items.description;
         temp["category"] = items.category;
         temp["price"] = items.price;
-        QJsonArray quanArr;
-        for(const int &iter : items.quantity){
+        QJsonArray quanArr;                                     //For an array within array case, Declare another json array obj
+        for(const int &iter : items.quantity){                  //similar discipline as the above for loop this time store in a variable with ur wanted type named iter
             QJsonObject qtemp;
             qtemp["quantity"] = iter;
-            quanArr.append(qtemp);
+            quanArr.append(qtemp);                              //Push the array element into Json Array object
         }
-        temp["quantity"] = quanArr;
-        menuArr.append(temp);
+        temp["quantity"] = quanArr;                             //Store the entire json array object as an element
+        menuArr.append(temp);                                   //Push the array element into Json Array object
     }
-    json["menu"] = menuArr;
+    json["menu"] = menuArr;                                     //Finally Store the entire Big array object into a json object to pass it into the file.
 }
--- TestJson/main.cpp
+++ TestJson/main.cpp
@@ -17,25 +17,25 @@ int main(int argc, char *argv[])
     engine.load(url);
 
 
-
+//////////////////////////////////////////////////////////////////////////////////////
 
 
 
     jsonable test;
-
-    QFile loadFile(QStringLiteral("menu.json"));
-   if (!loadFile.open(QIODevice::ReadOnly)) {
+////Loading save File process
+    QFile loadFile(QStringLiteral("menu.json"));    //Load named file
+   if (!loadFile.open(QIODevice::ReadOnly)) {       //Checking process watch the warning window to see notification
         qWarning("Couldn't open save file.");
         return false;
     }
-    QByteArray saveData = loadFile.readAll();
-    QJsonDocument loadDoc(QJsonDocument::fromJson(saveData));
-    test.read(loadDoc.object());
-    loadFile.close();
+    QByteArray saveData = loadFile.readAll();      //Read the file and save it into a temp var
+    QJsonDocument loadDoc(QJsonDocument::fromJson(saveData));   //Load the temp into a Json Document form in order to pass it as a JSON object
+    test.read(loadDoc.object());                    //Use the read function
+    loadFile.close();                               //Close the file
+/////Loading save File process
 
 
-
-
+/////Testing
 
 //    for(int i=0;i<10;i++){
         test.name="Buffalo wings";
@@ -48,19 +48,19 @@ int main(int argc, char *argv[])
         menu.push_back(test);
 //    }
 
+/////Testing
 
 
-
-    QFile saveFile(QStringLiteral("menu.json"));
-    QJsonObject gameObject;
-    if (!saveFile.open(QIODevice::WriteOnly)) {
+    QFile saveFile(QStringLiteral("menu.json"));    //Get the file u want to save
+    QJsonObject gameObject;                         //declare a Json OBJ var to store
+    if (!saveFile.open(QIODevice::WriteOnly)) {     //check the file to see if its open
         qWarning("Couldn't open save file.");
         return false;
     }
-    test.write(gameObject);
-    QJsonDocument saveDoc(gameObject);
-    saveFile.write(saveDoc.toJson());
-    saveFile.close();
+    test.write(gameObject);                         //Use the Defined write function in order to store the data into the declared jsonObject
+    QJsonDocument saveDoc(gameObject);              //Turns it into JsonDocument
+    saveFile.write(saveDoc.toJson());               //write the Document into the file
+    saveFile.close();                               //Close the file
 
 
 
